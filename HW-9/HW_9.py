13b82bdb11a502b287dbc46d78633357f21898ba
IvAn <irisus.ivuk@mail.ru>
2023-03-31 17:14:19 +0600

    Complete all tasks from HW_9

--- HW-9/HW_9.py
+++ HW-9/HW_9.py
@@ -1,21 +1,32 @@
+import json
+import re
+
 import requests
 
 
 
-def get_json():
-    params = {"tags":"/commeтts"}
-    headers ={
-        "Accept":"application/xml"
-    }
-    url=("https://jsonplaceholder.typicode.com")
-    r = requests.get(url, params=params, headers=headers)
+def func_9_1_3():
+    Burl = "https://habrahabr.ru/"
+    response = requests.get(f"{Burl}")
+    pattern_link = r'\<a href=\"(/[\w/]*)\"'
+    lst = []
+    for item in re.findall(pattern_link, str(response.content)):
+        lst.append(item)
+        with open("habr.txt", 'a') as fp:
+            fp.write("%s\n" % item)
+    print("Все ссылки сохранены в файле habr.txt")
 
-    print(r.status_code)
-    print(r.headers)
-    print(r.content)
+def get_json(base_url):
+    response = requests.get(f"{base_url}/comments")
+    write_to_file_data("response.json",json.dumps(response.json(),sort_keys=True,indent=4))
+    headers_dict = {}
+    for key,value in response.headers.items():
+        headers_dict[key] = value
+        print("{}:|| {}\n".format(key,value))
+
 
 def write_to_file_data(file,content,mode="w"):
-    print("Writing data!")
+    # print("Writing data!")
     with open(file,mode = mode,) as f:
         return f.write(content)
 def read_the_file_data(file):
@@ -31,7 +42,7 @@ def func_9_1_1():
         content = str(input("Введите текст который хотите видеть в файле:"))
         сontenter = content + "a\n"
         print(сontenter)
-        write_to_file_data("test.txt",сontenter,mode="a")
+        write_to_file_data("test.txt",сontenter)
     elif(choice == 2):
         print(read_the_file_data("test.txt"))
 
@@ -57,8 +68,10 @@ def main ():
         func_9_1_1()
     elif (choice == 2):
         print("\n Реализовать следующую логику: получать при помощи requests данные сервиса https://jsonplaceholder.typicode.com/(сущность можно выбрать любую, например https://jsonplaceholder.typicode.com/comments),выводить в консоль все пары заголовки, сохранять полученный json в файл на диск.\n")
-        get_json()
-
+        get_json("https://jsonplaceholder.typicode.com")
+    elif (choice == 3):
+        print("\n Обратиться с странице https://habrahabr.ru/. Получить текст страницы. При помощи регулярных выражений нужно получить все ссылки со страницы на другие.\n")
+        func_9_1_3()
     elif(choice == 8):
         print("\n При помощи reduce посчитать количество букв в словах: ['some', 'other', 'value'] \n")
 
